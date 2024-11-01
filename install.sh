9093f1cc2dd55580a96dd096121bbdb19ac37298
Alex Potapov <potapov@yahoo.com>
2024-10-31 21:46:27 -0400

    Initial

--- install.sh
+++ install.sh
@@ -75,6 +75,7 @@ EOF
 
 chmod 400 /root/.ssh/id_rsa
 
+ssh -T git@github.com
 git clone git@github.com:pilet-io/host-cli.git
 #mv /root/host-cli /root/cli
 #echo "export PATH=\$PATH:/root/cli" >> /root/.profile
