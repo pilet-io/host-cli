226ec0f200fe330ae2fa88ee146b58f0ab68cba0
Alex Potapov <potapov@yahoo.com>
2024-11-06 07:58:52 -0500

    kube

--- .bin/setup.sh
+++ .bin/setup.sh
@@ -2,7 +2,7 @@
 
 apt update
 
-git clone git@github.com:pilet-io/host-cli.git
+git clone https://github.com/pilet-io/host-cli.git
 mv /root/host-cli /root/cli
 
 sed -i '/export PATH=\$PATH:\/root\/cli' /root/.profile
